7577daa3ee927df78dbbd0aa761a4c48b3019015
Dominic Jodoin <dominic@travis-ci.com>
2018-02-22 15:51:39 -0500

    test for true being thruthy fun

--- test.js
+++ test.js
@@ -1,24 +1,5 @@
-const { spawn } = require('child_process');
-const request = require('request');
 const test = require('tape');
 
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
-
 test('responds to requests', (t) => {
-  t.plan(2);
-
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
-
-      t.false(error); // test 1
-      t.equal(response.statusCode, 200); // test 2
-    }); //end request
-	
-  }); //end child
-}); //end test
+  t.true(true)
+});
